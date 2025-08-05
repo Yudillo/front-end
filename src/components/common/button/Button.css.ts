273f2e7ce6d6b9104a826f89c7663cef30d62b06
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2025-08-05 11:38:17 +0900

    design: Button 컴포넌트 vanilla-extract recipes variant 적용

--- src/components/common/button/Button.css.ts
+++ src/components/common/button/Button.css.ts
@@ -1,5 +1,6 @@
 import { defaultTheme } from '@/styles/theme.css';
 import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 export const buttonWrapper = style({
   padding: '12px',
@@ -7,10 +8,24 @@ export const buttonWrapper = style({
   justifyContent: 'center',
 });
 
-export const button = style({
-  width: '100%',
-  height: '3.5rem',
-  borderRadius: defaultTheme.borderRadius.medium,
-  backgroundColor: defaultTheme.color.lightBlue,
-  color: defaultTheme.color.white,
+export const button = recipe({
+  base: {
+    width: '100%',
+    height: '3.5rem',
+    borderRadius: defaultTheme.borderRadius.medium,
+  },
+  variants: {
+    backColor: {
+      basic: { backgroundColor: defaultTheme.color.lightBlue },
+      signup: { backgroundColor: defaultTheme.color.lightGrey50 },
+    },
+    color: {
+      basic: { color: defaultTheme.color.white },
+      signup: { color: defaultTheme.color.basic },
+    },
+  },
+  defaultVariants: {
+    backColor: 'basic',
+    color: 'basic',
+  },
 });
--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -4,12 +4,14 @@ import * as s from './Button.css';
 interface ButtonProps {
   title: string;
   type: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+  backColor?: 'basic' | 'signup';
+  color?: 'basic' | 'signup';
 }
 
-export default function Button({ title, type }: ButtonProps) {
+export default function Button({ title, type, backColor, color }: ButtonProps) {
   return (
     <div className={s.buttonWrapper}>
-      <button className={s.button} type={type}>
+      <button className={s.button({ backColor, color })} type={type}>
         {title}
       </button>
     </div>
